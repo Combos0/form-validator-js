7c78869952037015b1df7b6f26738a8bccc171f3
Combos0 <jcx429@gmail.com>
2022-11-30 14:24:02 -0500

    refocused project to use webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const { builtinModules } = require('module');
+const path = require('path');
+HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+  entry: {
+    index: './src/app.js',
+    loader: './src/loader.js',
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Free Pictures of Dogs',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
