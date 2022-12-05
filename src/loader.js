476ee7a58e9f3e2b212d45c4f026e0761959979f
Combos0 <jcx429@gmail.com>
2022-12-04 19:40:34 -0500

    fleshing out main page

--- src/loader.js
+++ src/loader.js
@@ -4,6 +4,7 @@ export default function initialLoader() {
     const root = document.body;
 
     createHeader();
+    createNavBar();
     createDailyMessage();
     createImageSpace();
     createFormSpace();
@@ -19,12 +20,57 @@ export default function initialLoader() {
         const formSpace = document.createElement('div');
         formSpace.setAttribute('id', 'form-space');
         root.appendChild(formSpace);
+
+        const formTitle = document.createElement('div');
+        formTitle.setAttribute('id', 'form-title');
+        formTitle.textContent = 'Want to know when more pictures of Ajax drops? Sign up for notifications below!';
+        formSpace.appendChild(formTitle);
+
+        const form = document.createElement('form');
+        form.setAttribute.setAttribute('id', 'subscribe-form');
+        formSpace.appendChild(form);
+
+        const formFieldSet = document.createElement('fieldset');
+        form.appendChild(formFieldSet);
+
+        const nameLabel = document.createElement('label');
+        nameLabel.textContent = 'Name:';
+        formFieldSet.appendChild(nameLabel);
+
+        const nameInput = document.createElement('input');
+        formFieldSet.appendChild(nameInput);
+
+        const emailLabel = document.createElement('label');
+        emailLabel.textContent = 'Email:';
+        formFieldSet.appendChild(emailLabel);
+
+        const emailInput = document.createElement('input');
+        formFieldSet.appendChild(emailInput);
+
+        const countryLabel = document.createElement('label');
+        countryLabel.textContent = 'Country:';
+        formFieldSet.appendChild(countryLabel);
+
+        const countryInput = document.createElement('input');
+        formFieldSet.appendChild(countryInput);
+
+        const zipCodeLabel = document.createElement('label');
+        zipCodeLabel.textContent = 'Zip code:';
+        formFieldSet.appendChild(zipCodeLabel);
+
+        const zipCodeInput = document.createElement('input');
+        formFieldSet.appendChild(zipCodeInput);
     };
 
     function createImageSpace() {
         const imageSpace = document.createElement('div');
         imageSpace.setAttribute('id', 'image-space');
         root.appendChild(imageSpace);
+
+        const placeHolder = document.createElement('img');
+        placeHolder.setAttribute('src', '../images/IMG_0407.jpg');
+        placeHolder.setAttribute('id', 'place-holder');
+        imageSpace.appendChild(placeHolder);
     };
 
     function createHeader() {
@@ -40,4 +86,22 @@ export default function initialLoader() {
         dailyMessage.textContent = 'NOW SELLING DOG $9.95 ALL SALES FINAL!';
         root.appendChild(dailyMessage);
     };
-}
+
+    function createNavBar() {
+        const navBar = document.createElement('div');
+        navBar.setAttribute('id', 'nav-bar');
+        root.appendChild(navBar);
+
+        const homeBTN = document.createElement('button');
+        homeBTN.setAttribute('type', 'button');
+        homeBTN.setAttribute('id', 'home-BTN');
+        homeBTN.textContent = 'Home';
+        navBar.appendChild(homeBTN);
+
+        const aboutBTN = document.createElement('button');
+        aboutBTN.setAttribute('type', 'button');
+        aboutBTN.setAttribute('id', 'about-BTN');
+        aboutBTN.textContent = 'About';
+        navBar.appendChild(aboutBTN);
+    };
+};
